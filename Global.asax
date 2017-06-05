73689193d28581ddf5fcceb90766579631c7bb61
Andrii Boiko <vinil2001@ukr.net>
2017-06-05 14:42:55 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRMdemo.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using WebMatrix.WebData;
+using CRMdemo.Models;
+
+
+namespace CRMdemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "Users", "Id", "Login", autoCreateTables: true);
+            //ImportGoods.getImportedProductList();
+            //ImportClient.GetImportedAgentList();
+            //ImportUnits.GetAll();
+            //CompanyType.BaseInitializeCompanyTypes();
+
+
+            // Roles.AddUserToRole("sdf", "Admin");
+            //if (!Roles.RoleExists("Admin"))
+            //    Roles.CreateRole("Admin");
+            //if (!Roles.RoleExists("User"))
+            //    Roles.CreateRole("User");
+            // WebSecurity.CreateUserAndAccount("Admin", "qwerty");
+            //  WebSecurity.Logout();
+        }
+    }
+}
